59beaee3ed2ec7bfb96583b64caf4a8dac3e5488
Sanjeev1008yadav <130984035+Sanjeev1008yadav@users.noreply.github.com>
2023-04-18 18:45:56 +0530

    tag and class

--- dom.js
+++ dom.js
@@ -24,15 +24,40 @@
 //console.log(headerTitle.innerText);
 // headerTitle.innerHTML = '<h3>Hello</h3>';
 //headerTitle.style.borderBottom = 'solid 3px #000';
-var items= document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[2].textContent = 'Item 3';
-items[0].style.fontWeight = 'bold';
-items[1].style.fontWeight = 'bold';
-items[2].style.fontWeight = 'bold';
-items[3].style.fontWeight = 'bold';
-items[2].style.backgroundColor = 'green';
+// var items= document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[2].textContent = 'Item 3';
+// items[0].style.fontWeight = 'bold';
+// items[1].style.fontWeight = 'bold';
+// items[2].style.fontWeight = 'bold';
+// items[3].style.fontWeight = 'bold';
+// items[2].style.backgroundColor = 'green';
+
+// for(var i=0; i< items.length; i++){
+//     items[i].style.backgroundColor = '#f4f4f4';
+// }
+// GETELEMENTBYTAGNAME
+var li= document.getElementsByTagName('li');
+console.log('li');
+console.log(li[1]);li[2].textContent = 'Item 3';li[0].style.fontWeight = 'bold';
+li[1].style.fontWeight = 'bold';
+li[2].style.fontWeight = 'bold';
+li[3].style.fontWeight = 'bold';
+li[2].style.backgroundColor = 'green';
+
+var list4=document.getElementsByClassName("xyz");
+// console.log(list4);
+list4[0].style.backgroundColor="red";
+list4[0].style.width="60px"
+
+
+
+
+
+
+
+
 
 
 
